6424e79144e71f6adb5dafca369f84bd98639578
Jawad Shaikh <J.shk1980@gmail.com>
2024-05-30 22:56:00 +0000

    Added more Text on Home page and typewriter effect

--- portfolio-3.0/components/Hero.tsx
+++ portfolio-3.0/components/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { TypewriterEffect } from "./ui/typewriter-effect";
+import { TypewriterEffect, TypewriterEffectSmooth } from "./ui/typewriter-effect";
 
 export default function TypewriterEffectDemo() {
   const words = [
@@ -24,9 +24,54 @@ export default function TypewriterEffectDemo() {
       className: "text-blue-500 dark:text-blue-500",
     },
   ];
+  const moreWords = [
+    {
+      text: "I"
+    },{
+      text: "Am"
+    },
+    {
+      text: "A"
+    },
+    {
+      text: "Software Developer",
+      className: "text-blue-500 dark:text-blue-500"
+    }
+  ]
+  const EvenMoreWords = [
+    {
+      text: "I"
+    },{
+      text: "Am"
+    },
+    {
+      text: "A"
+    },
+    {
+      text: "Problem Solver",
+      className: "text-blue-500 dark:text-blue-500"
+    }
+  ]
+  const AddiotonallyEvenMoreWords = [
+    {
+      text: "I"
+    },{
+      text: "Am"
+    },
+    {
+      text: "An"
+    },
+    {
+      text: "Innovator",
+      className: "text-blue-500 dark:text-blue-500"
+    }
+  ]
   return (
     <div className="flex bg-transparent flex-col items-center justify-center h-[40rem] ">
-      <TypewriterEffect words={words} />
+      <TypewriterEffectSmooth words={words} />
+      <TypewriterEffectSmooth words={moreWords}/>
+      <TypewriterEffectSmooth words={EvenMoreWords}/>
+      <TypewriterEffectSmooth words={AddiotonallyEvenMoreWords}/>
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
       </div>
     </div>
